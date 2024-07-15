ab4f5f6c867fd55fe9b01197bee996d04d2077d3
Lexyth <marfy@web.de>
2024-07-15 03:25:04 +0200

    feat: Export useHandler, run, createHandler, Input, Button, Output, and App.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export { default as App } from "./App";
+
+export { Input } from "./components/Input";
+export { Button } from "./components/Button";
+export { Output } from "./components/Output";
+
+export { useHandler } from "./hooks/useHandler";
+export { run, createHandler } from "./util/ai";
