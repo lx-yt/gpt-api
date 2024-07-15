d4c95d793d32ec00f130279c70636b5194eb4313
Lexyth <marfy@web.de>
2024-07-15 03:52:17 +0200

    refactor: Add placeholder text

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,14 +3,14 @@ const logger = RootLogger.getLogger("Input");
 
 import React from "react";
 
-import type { HandlerWithSetPrompt } from "../hooks/useHandler";
+import type { ReactiveHandler } from "../hooks/useHandler";
 
 export function Input({
   handler,
   className,
   style,
 }: {
-  handler: HandlerWithSetPrompt;
+  handler: ReactiveHandler;
   className?: string;
   style?: React.CSSProperties;
 }) {
@@ -32,6 +32,7 @@ export function Input({
     <textarea
       value={prompt}
       onChange={handleChangePrompt}
+      placeholder="Enter your prompt here"
       className={className}
       style={style}
     ></textarea>
