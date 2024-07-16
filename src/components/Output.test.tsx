11cd70fb5410bf22fbef57d885ee590a91e89e0d
Lexyth <marfy@web.de>
2024-07-16 07:33:26 +0200

    test: Update tests

--- src/components/Output.test.tsx
+++ src/components/Output.test.tsx
@@ -1,18 +1,50 @@
-import { describe, expect, test } from "vitest";
+import { RootLogger } from "@lx-yt/logging";
+RootLogger.getLogger("Output").minLevel = "debug";
 
-import { render, screen, renderHook } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+
+import { act, cleanup, render, renderHook } from "@testing-library/react";
 
 import { useHandler } from "../hooks/useHandler";
 import { Output } from "./Output";
 
+const config = { llm: "gemini", model: "gemini-1.5-pro-latest" } as const;
+
 describe("Output", () => {
-  test.todo("should render", () => {
-    const { result } = renderHook(() =>
-      useHandler({ model: "gemini-1.5-pro-latest" })
-    );
-    render(<Output handler={result.current} />);
-    expect(screen.getByDisplayValue("")).toBeInTheDocument();
+  beforeEach(() => {
+    cleanup();
   });
 
-  test.todo("should change value when the handler is run");
+  test("should render", () => {
+    const { result } = renderHook(() => useHandler(config));
+    const { container } = render(<Output handler={result.current} />);
+    expect(container.children[0]).toBeInTheDocument();
+  });
+
+  test("should change value when the handler is run", async () => {
+    let handler = {} as ReturnType<typeof useHandler>;
+
+    function TestComponent() {
+      const usedHandler = useHandler(config);
+      handler = usedHandler;
+      return <Output handler={usedHandler} />;
+    }
+
+    const { container } = render(<TestComponent />);
+
+    expect(container.children[0]).toBeInTheDocument();
+    expect(container.children[0]).toContainHTML("");
+
+    await act(async () => {
+      await handler.run();
+    });
+
+    expect(handler.output).toEqual(
+      "Simulated call to llm 'gemini' model 'gemini-1.5-pro-latest' with prompt: ''"
+    );
+
+    expect(container.children[0]?.innerHTML).toEqual(
+      `<p>${handler.output}</p>`
+    );
+  });
 });
