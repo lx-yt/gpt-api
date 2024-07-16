6eb1f5ed05fa1d041b5435bf3ecb737ee3dcb558
Lexyth <marfy@web.de>
2024-07-16 07:08:03 +0200

    feat: Include config.llm in logging messages.

--- src/util/ai.ts
+++ src/util/ai.ts
@@ -18,14 +18,18 @@ export interface Handler {
 export const LLMS = ["gemini", "gpt", "claude"] as const;
 
 export async function run(prompt: string, config: Config) {
-  logger.info(`Running model ${config.model} with prompt: ${prompt}`);
+  logger.info(
+    `Running llm ${config.llm} model ${config.model} with prompt: ${prompt}`
+  );
 
   let result: string;
   if (import.meta.env.DEV) {
     logger.info("Simulating API call...");
     result = await new Promise((resolve) =>
       setTimeout(() => {
-        resolve(`Simulated call to '${config.model}' with prompt: '${prompt}'`);
+        resolve(
+          `Simulated call to llm '${config.llm}' model '${config.model}' with prompt: '${prompt}'`
+        );
       }, 1000)
     );
   } else {
