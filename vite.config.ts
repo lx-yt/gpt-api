c7b6797c46d2e0cfa68b80db699106b4b86e047f
Lexyth <marfy@web.de>
2024-07-24 06:00:30 +0200

    refactor: Use @lx-yt/ts-config-default, @lx-yt/vite-config-default, and @lx-yt/eslint-config-default

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,11 @@
-/// <reference types="vitest/config" />
+import { defineConfig, mergeConfig } from "vite";
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint2";
-import checker from "vite-plugin-checker";
-import dts from "vite-plugin-dts";
+// using relative path to avoid error: TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts"
+import baseConfig from "./node_modules/@lx-yt/vite-config-default/vite.config";
+import reactConfig from "./node_modules/@lx-yt/vite-config-default/vite.config.react";
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-    lib: {
-      entry: "src/index.ts",
-      name: "GPT-API",
-      fileName: "index",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-  plugins: [
-    react(),
-    eslint({
-      cache: false,
-      emitErrorAsWarning: true,
-      include: ["src/**/*.ts", "src/**/*.tsx"],
-      exclude: ["src/**/*.test.ts", "src/**/*.test.tsx"],
-    }),
-    checker({ typescript: true }),
-    dts({ rollupTypes: true }),
-  ],
-  server: {
-    open: true,
-    port: 5000,
-  },
-  test: {
-    environment: "jsdom",
-    setupFiles: ["./vitest-setup.ts"],
-  },
-});
+export default defineConfig(
+  mergeConfig(mergeConfig(baseConfig, reactConfig), {
+    build: { lib: { name: "GPT-API" } },
+  })
+);
